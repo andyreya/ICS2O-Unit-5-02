8a359bd01e12909da3fee6dd8431e1626d625d8e
andyreya <>
2021-06-08 14:36:09 +0000

    more spaces

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('button').addEventListener('click', clickHere) /* makes action occur when button is clicked */
 
-function clickHere() {
-  alert ('Hello World, its me :)')
+function clickHere () {
+  alert('Hello World, its me :)')
 } /*  alerts window "hello world its me"  */
